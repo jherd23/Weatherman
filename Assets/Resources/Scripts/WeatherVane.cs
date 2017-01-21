5f4214b0c73d54c341b502ae99121e6a2efac611
Soupdoop <jherd23@gmail.com>
2017-01-21 11:39:18 -0500

    Trying My Best™

--- Assets/Resources/Scripts/WeatherVane.cs
+++ Assets/Resources/Scripts/WeatherVane.cs
@@ -4,21 +4,72 @@ using UnityEngine;
 
 public class WeatherVane : Device {
 
-	float dir;
-	float lastdir;
+	Day.windDirection dir;
+
+	float seconds = 1;
+	bool thing = true;
 
 	// Use this for initialization
 	void Start () {
-		dir = 0;
+		dir = Day.windDirection.N;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		gameObject.transform.RotateAround (gameObject.transform.position,new Vector3(0,1),dir - lastdir);
-		lastdir = dir;
+		if (seconds > 0) {
+			seconds -= Time.deltaTime;
+		} else if(thing) {
+			rotate (90);
+			thing = false;
+		}
 	}
 
-	override void set(Day d) {
-		
+	public override void set(Day d) {
+		reverse ();
+		rotate (d.WindDirection);
+		dir = d.WindDirection;
+	}
+
+	int directionToDegrees(Day.windDirection r) {
+		switch (r) {
+			case Day.windDirection.N:
+				return 0;
+				break;
+			case Day.windDirection.NE:
+				return 45;
+				break;
+			case Day.windDirection.E:
+				return 90;
+				break;
+			case Day.windDirection.SE:
+				return 135;
+				break;
+			case Day.windDirection.S:
+				return 180;
+				break;
+			case Day.windDirection.SW:
+				return 215;
+				break;
+			case Day.windDirection.W:
+				return 270;
+				break;
+			case Day.windDirection.NW:
+				return 315;
+				break;
+			default:
+				return 0;
+				break;
+		}
+	}
+
+	void reverse () {
+		rotate (-1 * directionToDegrees (dir));
+	}
+
+	void rotate (Day.windDirection r) {
+		rotate (directionToDegrees (r));
+	}
+	void rotate (int degrees) {
+		gameObject.transform.RotateAround (gameObject.transform.position,new Vector3(0,1),degrees);
 	}
 }
