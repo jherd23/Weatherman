da748ac68007f5dca29c3f605fbe8979680c2329
Soupdoop <jherd23@gmail.com>
2017-01-21 10:57:22 -0500

    Added WeatherVane class -- Now with less bloat!

---
+++ Assets/Resources/Scripts/WeatherVane.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeatherVane : Device {
+
+	float dir;
+	float lastdir;
+
+	// Use this for initialization
+	void Start () {
+		dir = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.RotateAround (gameObject.transform.position,new Vector3(0,1),dir - lastdir);
+		lastdir = dir;
+	}
+
+	override void set(Day d) {
+		
+	}
+}
