f4deb3e9343fb4ec9e80868154d5a160e4054e8e
Soupdoop <jherd23@gmail.com>
2017-01-20 23:13:50 -0500

    Commit to it

---
+++ Assets/Scripts/Device.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Device : MonoBehaviour
+{
+
+	public int unlockSeason;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	public abstract void set (Day d);
+}
+
--- Assets/Scripts/WeatherController.cs
+++ Assets/Scripts/WeatherController.cs
@@ -4,13 +4,29 @@ using UnityEngine;
 
 public class WeatherController : MonoBehaviour {
 
+	public Device[] devices;
+
+	public int daysPerSeason;
+
+	public int numberOfSeasons;
+
+	Day[] days;
+
 	// Use this for initialization
 	void Start () {
-		
+		//days = generateDays() //TODO: make that
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	void setInstruments(Day d) {
+		for (int i = 0; i < devices.Length; i++) {
+			if (d.season >= devices[i].unlockSeason) {
+				devices [i].set (d);
+			}
+		}
 	}
 }
