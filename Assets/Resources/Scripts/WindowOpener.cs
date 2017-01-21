021b430d8bbaa464d0d18a066e284f3b63dc64e3
zsd4yr <zsd4yr@virginia.edu>
2017-01-21 15:48:33 -0500

    What a time to be alive

---
+++ Assets/Resources/Scripts/WindowOpener.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WindowOpener : MonoBehaviour {
+
+	public Slider control;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.position = new Vector3(gameObject.transform.position.x, control.value, gameObject.transform.position.z);
+	}
+}
