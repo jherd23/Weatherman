f9a9223b3dbe008821bd1b8677089c0f811f237c
sanjanamendu <sanjanamendu@gmail.com>
2017-01-22 10:36:52 -0500

    camera fixes

--- Assets/Resources/Scripts/CameraMovement.cs
+++ Assets/Resources/Scripts/CameraMovement.cs
@@ -32,13 +32,13 @@ public class CameraMovement : MonoBehaviour {
 			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 			if (Physics.Raycast(ray, out hit) && (hit.transform.parent != null)) {
 				obj = GameObject.Find(hit.transform.parent.name);
-				if (obj.name == "anemometer") {
-					targetpos = new Vector3 (obj.transform.position.x, obj.transform.position.y + 500, transform.position.z);
-					targetFOV = initFOV;
-				} else {
+//				if (obj.name == "anemometer") {
+//					targetpos = new Vector3 (obj.transform.position.x, obj.transform.position.y + 500, transform.position.z);
+//					targetFOV = initFOV;
+//				} else {
 					targetpos = new Vector3 (obj.transform.position.x, obj.transform.position.y + 200, transform.position.z);
 					targetFOV = 18;
-				}
+//				}
 //				if (obj.CompareTag ("clickable")) {
 					if (transform.position.x > obj.transform.position.x) {
 						dir = false;
