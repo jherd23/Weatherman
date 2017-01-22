89184e349a4b7a44666a73aa933fe457175b2b14
zsd4yr <zsd4yr@virginia.edu>
2017-01-21 21:20:31 -0500

    dat dank lowpass

--- Assets/Resources/Scripts/WindowOpener.cs
+++ Assets/Resources/Scripts/WindowOpener.cs
@@ -8,61 +8,91 @@ public class WindowOpener : MonoBehaviour {
 
 	public Slider control;
 
-	public AudioMixerGroup outside;
-	public AudioMixerGroup rainOutside;
-	public AudioMixerGroup windOutside;
-	public AudioMixerGroup otherOutside;
-	public AudioMixerGroup inside;
-	public AudioMixerGroup roof;
+	public bool Fair;
+	public bool Rain;
 
-	AudioSource ausOut;
-	AudioSource ausIn;
-	AudioSource ausRoof;
+	public AudioMixerGroup roof;
+	public AudioMixerGroup roofParent;
+	public AudioMixerGroup rain;
+	public AudioMixerGroup outside;
 
 	//gentle
-	AudioClip rain;
-	AudioClip waves;
-	AudioClip wind;
+	public AudioSource ausRain;
+	public AudioSource ausWaves;
+	public AudioSource ausWind;
 
 	//mean 
-	AudioClip windHowl;
-	AudioClip stormSea;
-	AudioClip stormWaves;
+	public AudioSource ausWindHowl;
+	public AudioSource ausStormSea;
+	public AudioSource ausStormWaves;
 
 	//roof
-	AudioClip roofRain;
+	public AudioSource ausRoofRain;
 
 	//window itself
-	AudioClip windowSqueak;
-	AudioClip windowShut;
+	public AudioSource ausWindowSqueak;
+	public AudioSource ausWindowShut;
 
 	// Use this for initialization
 	void Start () {
-		ausOut = new AudioSource ();
-		ausOut.outputAudioMixerGroup = outside;
 
-		ausIn = new AudioSource ();
-		ausIn.outputAudioMixerGroup = roof;
+		//setup sources
 
-		ausRoof = new AudioSource ();
-		ausRoof.outputAudioMixerGroup = inside;
-
-		rain = Resources.Load ("Audio/LightRain") as AudioClip;
-		waves = Resources.Load ("Audio/WavesGentle") as AudioClip;
-		wind = Resources.Load ("Audio/Wind+Crickets") as AudioClip;
-
-		windHowl = Resources.Load ("Audio/WindHowl") as AudioClip;
-		stormSea = Resources.Load ("Audio/StormSea") as AudioClip; 
-		stormWaves = Resources.Load ("Audio/Stormwaves") as AudioClip;
-
-		roofRain = Resources.Load ("Audio/RainRoof") as AudioClip;
-
-		windowSqueak = Resources.Load ("Audio/WindowSqueak") as AudioClip;
-		windowShut = Resources.Load ("Audio/WindowShut") as AudioClip;
+		DisableAll ();
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		gameObject.transform.position = new Vector3(gameObject.transform.position.x, control.value, gameObject.transform.position.z);
+		outside.audioMixer.SetFloat ("cutoffOutside", 2102.1486f * Mathf.Log ((control.value - 539.0f)) + 9000.0f);
+		roof.audioMixer.SetFloat ("cutoffOutside", 2102.1486f * Mathf.Log ((control.value - 539.0f)) + 9000.0f);
+
+		outside.audioMixer.SetFloat("volOutside", ((6.0f/484.0f) * (control.value - 540.0f)) - 6.00f);
+		roofParent.audioMixer.SetFloat("volRoofParent", ((6.0f/484.0f) * (control.value - 540.0f)) - 6.00f);
+
+		if (Fair) {
+			ausWaves.volume = 1.0f;
+			ausWind.volume = 1.0f;
+			ausWindHowl.volume = 0.0f;
+			ausStormSea.volume = 0.0f;
+			ausStormWaves.volume = 0.0f;
+		} else {
+			ausWindHowl.volume = 1.0f;
+			ausStormSea.volume = 1.0f;
+			ausStormWaves.volume = 1.0f;
+			ausWaves.volume = 0.0f;
+			ausWind.volume = 0.0f;
+		}
+
+		if (Rain) {
+			ausRoofRain.volume = 1.0f;
+			ausRain.volume = 1.0f;
+			if (Fair) {
+				rain.audioMixer.SetFloat ("volRainOutside", 9);
+				roof.audioMixer.SetFloat ("volRoof", 4);
+			} else {
+				rain.audioMixer.SetFloat ("volRainOutside", 15);
+				roof.audioMixer.SetFloat ("volRoof", 14);
+			}
+		} else {
+			ausRoofRain.volume = 0.0f;
+			ausRain.volume = 0.0f;
+		}
 	}
+
+	void DisableAll (){
+		ausRain.volume = 0.0f;
+		ausWaves.volume = 0.0f;
+		ausWind.volume = 0.0f;
+
+		ausWindHowl.volume = 0.0f;
+		ausStormSea.volume = 0.0f;
+		ausStormWaves.volume = 0.0f;
+
+		ausRoofRain.volume = 0.0f;
+
+		ausWindowSqueak.volume = 0.0f;
+		ausWindowShut.volume = 0.0f;
+	}
+	//2102.1486 * log(x - 539) + 9000
 }
